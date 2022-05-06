44c41aa390cc8a79cecfc035a5b79bfb24441217
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2022-05-06 19:05:47 +0630

    #1 deev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel devlop
                 </div>
 
                 <div class="links">
