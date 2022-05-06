e6d82ad7b441c59d11d3ac361d41bcdcf961ae7e
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2022-05-06 19:41:56 +0630

    develop2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Develop2
                 </div>
 
                 <div class="links">
